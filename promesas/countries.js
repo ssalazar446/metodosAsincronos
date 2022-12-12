69a51c9b7d9c1d9660d6fcf71f7454deca9c57d5
ssalazar446 <ssalazar446@misena.edu.co>
2022-12-12 10:54:43 -0500

    API countries

--- promesas/countries.js
+++ promesas/countries.js
@@ -9,9 +9,13 @@ function setCountries () {
     .then(resp => resp.json()) //Se convierte la respuesta a JSON
     .then(data => {             //Se obtiene los datos en formato JSON
         let htmlOptions = "";
-        for (let item of data) {
-            document.getElementById("lista_paises").innerHTML += `<option value="${item.ccn3}">${item.name.common}</option>    `
-        }
+        // for (let item of data) {
+        //     document.getElementById("lista_paises").innerHTML += `<option value="${item.ccn3}">${item.name.common}</option>    `
+        // }
+        data.map((item)=> {
+            return htmlOptions += `<option value="${item.ccn3}">${item.name.common}</option>`
+        });
+        document.getElementById("lista_paises").innerHTML = htmlOptions;
     })
 }
 
