bdab0a3b9693e76391e02fc2ff8ae76dc10d0776
ssalazar446 <ssalazar446@misena.edu.co>
2022-12-12 10:45:29 -0500

    consum API

---
+++ promesas/countries.js
@@ -0,0 +1,20 @@
+document.querySelector("#lista_paises").addEventListener('change', infocountry);
+const url = 'https://restcountries.com/v3.1';
+setCountries();
+
+
+function setCountries () {
+    console.log('Set countries V3.1')
+    fetch(`${url}/all`)
+    .then(resp => resp.json()) //Se convierte la respuesta a JSON
+    .then(data => {             //Se obtiene los datos en formato JSON
+        let htmlOptions = "";
+        for (let item of data) {
+            document.getElementById("lista_paises").innerHTML += `<option value="${item.ccn3}">${item.name.common}</option>    `
+        }
+    })
+}
+
+function infocountry() {
+    console.log("Info Pais...")
+}
